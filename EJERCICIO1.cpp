612eae708558aae008dbcca5c37228cf05d76ac2
Ricardofloressgit <155786348+Ricardofloressgit@users.noreply.github.com>
2024-01-29 23:22:15 -0400

    Add files via upload

---
+++ EJERCICIO1.cpp
@@ -0,0 +1,145 @@
+/*Titulo :PRACTICO N#1
+Autor : Ricardo flores soruco
+Descripcion:DEFINICIONES DE ASIGNACIONES
+Fecha: 2024-01-16
+							ANALISIS
+DEFINICION DEL PROBLEMA : se debe de realizar un programa en el que muestre si los valores asignados estan dentro de sus parametros establecidos
+y mostrar la convercion si es correcta o incorrecta y comentar el porque es correcta o incorrecta
+
+DATOS DE ENTRADA:Se solicitara al usuario ingresar la opcion que desee visualizar 
+
+INFORMACION DE SALIDA :Se mostrar en pantalla el resultado,la convercion si es correcta o incorrecta y  su comentario del porque.
+
+VARIABLES: Se definiran las variables de tipos: Int n1,n2,resp;,unsigned int nn1,nn2;,double d1,d2;,char c1;,bool b1;//las variables ya fueron definidas 
+							DISEÑO
+EL PROBLEMAS SE DIVIDIRA EN PEQUEÑOS PROBLEMAS PARA PODER REALIZARLO PASO A PASO :
+
+-Se hara uso de las siguientes libreriaas #include <iostream>,#include <conio.h> para empezar el proceso
+
+-las variables ya fueron definidas de tipos y valores.
+
+-Se hara uso del ciclo DO WHILE para que las opciones se muestren cuantas veces el usuario lo desee
+
+- Se imprimira el nombre del estudiante,posteriormente se montrara las opciones disponibles para poder visualizar los resultados
+
+-Se imprimera los resultados de la opcion que el usuario haiga elegido
+
+- Para serar el programa se utilizara 0 cualquier otra tecla que no se muestre en el menu, retornara al menu principal
+
+*/
+#include <iostream>// Libreria estandar de C++ en la cual se puede usar las entradas y salidas de datos e informacion
+#include <conio.h>// es necesario para usar el esperar tecla 
+using namespace std;// el uso de espacio de nombre para STD de la I/O
+
+
+	int main(int argc, char *argv[]) 
+	{
+		
+		/* los parametros de los tipos de datos:
+		valores constantes 
+		int = -2,147,483,648 a 2,147,483,647
+		unsigned int= 0 a 4,294,967,295
+		double = 1.7 x 10-308 a 1.7*10+308(15 dec 
+		char = -128 a 127	
+		bool = 0 si es falso y cualquier numero difernte de 0 es positivo
+		*/
+		int n1,n2;
+		unsigned int nn1,nn2;
+		double d1,d2,primer,segundo,tercero_char ;
+		char c1,resp;
+		bool b1;
+		//variables con parametros establecidos
+		primer = 2147483647;
+		segundo = 42949672995;
+		tercero_char = 127;
+		// Aaignacion de valores 
+		n1=2147483645;//correcto //esta dentro del parametro
+		nn1=n1+3;//correcto //esta dentro del parametro
+		d1=c1+n1;//incorrecto//esta dentro del parametro
+		c1=n1+2;//incorrecto // mustra vacio ¿porque?
+		b1=-1000;//correcto // su respuesta es positivo ya que//para ser positivo debe de ser diferente de cero
+		/*PRUEBA PARA VISUALIZAR EL RESULTADO
+		cout<<n1<<" "<<"1"<<endl;
+		cout<<nn1<<" "<<"2"<<endl;
+		cout<<d1<<" "<<"3"<<endl;
+		cout<<c1<<" "<<"4"<<endl;
+		cout<<b1<<" "<<"5"<<endl;*/
+		do{
+			
+			cout<< "ALUMNO : Ricardo Flores Soruco "<<endl<<endl;
+			cout<<"ingrese un numero"<<endl<<endl;
+			cout<<"1: Ver resultado de la primera asignacion n1 "<<endl<<endl;
+			cout<<"2: Ver resultado de la segunda asignacion nn1 "<<endl<<endl;
+			cout<<"3: Ver resultado de la tercera asignacion d1 "<<endl<<endl;
+			cout<<"4: Ver resultado de la cuarta asignacion c1 "<<endl<<endl;
+			cout<<"5: Ver resultado de la Quinta asignacion b1 "<<endl<<endl;
+			cout<<"0:salir"<<endl;
+			cin>>resp;
+				switch(resp){
+					
+					case '1': 
+					cout<<"el resultado de la primera asignacion es : "<<endl;//imprimira el encabezado
+					cout<<"Resultado"<<"     "<<"coversion"<<endl;//simula las cabezeras de la tabla
+					cout<<"================================="<<endl;// solo es un separador como una linea horizontal
+					cout<<n1<<"    "<<"CORRECTO"<<endl<<endl;// se mostrara el contenido de la variable. espacio y a direccion de conversion a lineas de abajo mostrara el resultado 
+					cout<<"El resultado es correcto ya que el valor asignado no excede de los parametros establecidos en el tipo de dato de int "<<endl;//comentario o el porque de la conversion
+					_getch();//esperar tecla  
+					system("cls");//limpiar pantalla
+			
+					break;
+					case '2': cout<<"el resultado de la segunda asignacion es : "<<endl;
+					cout<<"Resultado"<<"     "<<"coversion"<<endl;
+					cout<<"================================="<<endl;
+					cout<<nn1<<"    "<<"CORRECTO"<<endl<<endl;
+					cout<<"El resultado es correcto ya que el valor asignado no excede de los parametros establecidos en el tipo de dato de unsigned int "<<endl;
+					_getch();
+					system("cls");
+					break;
+					case '3': cout<<"el resultado de la tercera asignacion es : "<<endl;
+					cout<<"Resultado"<<"     "<<"coversion"<<endl;
+					cout<<"================================="<<endl;
+					cout<<d1<<"    "<<"CORRECTO"<<endl<<endl;//la variable c1 no contiene valor 
+					cout<<"El resultado es correcto, el valor esta dentro del los parametros establecidos en el tipo de dato de double "<<endl;
+					cout<<"por motivo que la variable c1 no esta inicializada, al no estar inicializada Habría un comportamiento indefinido en C++"<<endl;
+					cout<<"el valor específico de c1 no se ha proporcionado y es indeterminado. El resultado : 2.14748e+009"<<endl; 
+					cout<<"es simplemente la representación en notación científica del valor numérico resultante de la operación,"<<endl;
+					cout<<"basado en la indeterminación de c1 y la conversión implícita a int antes de la suma..";
+					_getch();
+					system("cls");
+					break;
+					case '4': cout<<"el resultado de la cuarta asignacion es : "<<endl;
+					cout<<"Resultado"<<"     "<<"coversion"<<endl;
+					cout<<"================================="<<endl;
+					cout<<c1<<"             "<<"INCORRECTO"<<endl<<endl;
+					cout<<"El resultado es incorrecto ya que el valor asignado excede a los parametros establecidos en el tipo de dato de char "<<endl;
+					cout<<"Se realza la operacion modulo la cual se realizara de la siguiente forma : 2147483647 % 256 "<<endl;
+					cout<<"el residuo sera : 255\nlo cual char adaptara a esas cantidad dentro de su parametros llegando a sus limites ida y vuelta"<<endl;
+					cout<<"por ende el resultado caera en el 0 el cual el cero en la tabla ASCII es un caracter nulo";
+					_getch();
+					system("cls");
+					break;
+					case '5': cout<<"el resultado de la Quinta asignacion es : "<<endl;
+					cout<<"Resultado"<<"     "<<"coversion"<<endl;
+					cout<<"================================="<<endl;
+					cout<<"  "<<b1<<"           "<<"CORRECTO"<<endl<<endl;
+					cout<<"El resultado es positivo ya que en el tipo de datos bool si el numero asignado es diferente de cero sera positivo o VERDADERO"<<endl;
+					_getch();
+					system("cls");
+					break;
+					case '0': cout<<"opcion salir: "<<endl<<endl;
+					cout<<"Finalizando programa "<<endl;
+					break;
+					default: cout<<"selecione una opcion disponible "<<endl<<endl;
+					cout<<"presione culaquier tecla para regresar "<<endl;
+					_getch();
+					system("cls");
+					break;
+					};
+					
+				}while(resp!='0');
+		
+		
+		
+		return 0; 
+}
+
---
+++ EJERCICIO2.cpp
@@ -0,0 +1,151 @@
+/*Titulo :practico 1
+Autor : Ricardo flores soruco
+Descripcion: EJERCICIO 2,MENU.
+Fecha: 2024-01-16
+								ANALISIS
+
+DEFINICION DEL PROBLEMA: se debe de crear un programa que funcione como menu el cual tenga las opciones de multiplicar
+de dos y tres numeros, division de dos numeros, indicar el numeros mayor de 3 numeros,y solo se podra cerrar el programa con la tecla 's' o 'S'
+
+DATOS DE ENTRADA : Se solicitara al usuario que ingrese a una de las opciones disponibles, se pedira ingresar datos numericos dos o tres numeros,
+
+INFORMACION DE SALIDA : Se imprimira en pantalla los resultado que el usuario hayga escogido realizar.
+
+VARIABLE : Se utilizaran las variables opcion, minuscula,COMO CHAR,las variables numero1,numero2,numero3,resultado,mayor;COMO DOUBLE
+
+								DISEÑO
+
+EL PROBLEMAS SE DIVIDIRA EN PEQUEÑOS PROBLEMAS PARA PODER REALIZARLO PASO A PASO :
+
+- Se arran uso de las siguientes bibliotecas #include <iostream>, #include <conio.h> ,#include <cctype>
+
+-#include <iostream> : Biblioteca estandar de C++ para poder utilizar las entradas y salida de datos
+
+-#include <conio.h>  : Se utilizara para poder utilizar la function esperar tecla 
+
+-#include <cctype>	 : Se utilizara para poder llamar a la funcion TOLOWER,
+ la cual nos permite que el usuario puede colocar alguna letra y esta funcion aceptara tanto minuscula como mayuscula
+
+-Se declararan variables de tipo char y double. las cuales las de tipo char seran las que se manejen en la funsion SWITCH y las DOUBLE para almacenar los numeros ingresados o resultados
+
+-Se imprimira en pantalla las opciones disponible del menu,posteriormente la opcion elegida por el usuario se guardara en la variable minuscula
+
+-Se hara uso del ciclo DO WHILE la cual nos permitira repetir las opciones del menu hasta que el usuario decida dar por terminado el programa 
+
+-Se hara uso de la funcion SWITCH para poder mostrar las opciones disponibles del menu 
+- Para la multiplicación se realizaran de la siguiente forma numero1*numero2 y numero3 en caso de tres numeros
+
+- Para la division se realizaran de la siguiente forma el primer numero ingresado sera el dividendo y el segundo el divisor
+se agregara la funcion if = para mostrar el resultado,else para generar un mensaje de error si se coloca al divisor 0 
+
+-Para calcular el mayor de 3 número se utilizara la variable mayor en la cual se compararan con el signo del > y se hara uso del signo de ?
+El signo de interrogación ? se utiliza como parte del operador ternario.
+
+-Para salir se esta utilizando la funcion tolower las cual nos permite que la letra S sea aceptada como mayuscula o minuscula
+
+PARA PULIR EL PROGRAMA :
+se estan usando las funciones de _getch la cual nos permite esperar tecla y syste(cls)para limpiar pantalla
+*/
+#include <iostream>//Biblioteca estandar de C++
+#include <conio.h>// necesario para utilizar el _getch()
+#include <cctype> // para utilizar el tolower. nota: tambien se puede utilizar sin hacer uso de la libreria, pero lo recomendable es usar la libreria 
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+	char opcion,minuscula;//estas variables solo seran usadas para la funcion switch
+	double numero1,numero2,numero3,resultado,mayor;
+	opcion = 0;//se inicializo en cero para que muestre la advertencia que aparecia "opcion no se esta inicializando para asignar a minuscula"
+	
+	do{
+		minuscula = tolower(opcion);//Permitira que el usuario pueda salir con la letra S o s
+		cout<<"ALUMNO : RICARDO FLORES SORUCO "<<endl<<endl;
+		cout<<"   "<< "MENU"<<endl<<endl;
+		cout<< "1:Multiplicación con 2 números "<<endl<<endl;
+		cout<< "2:División con 2 números "<<endl<<endl;
+		cout<< "3:Multiplicación con 3 números"<<endl<<endl;
+		cout<< "4:Mayor de 3 números "<<endl<<endl;
+		cout<< "S:Salir"<<endl;
+		cin>>minuscula;
+		
+		switch(minuscula){
+		case '1': 
+			system("cls");//sirve para limpiar pantalla no es necesario libreria.
+			cout<<"   "<<"MULTIPLICACION "<<endl<<endl;
+			cout<<"ingrese el primer número "<<endl;
+			cin>>numero1;
+			cout<<"ingrese el segundo número "<<endl;
+			cin>>numero2;
+			resultado = numero1*numero2;
+			cout<<"El resultado de la multiplicacion es de :"<<resultado<<endl<<endl;
+			cout<<"Presion cualquier tecla para regresar al menu ";
+			_getch();
+			system("cls");
+			break;
+		case '2':
+			system("cls");
+			cout<<"   "<<" DIVISION "<<endl<<endl;
+			cout<<"ingresa el dividendo "<<endl;
+			cin>>numero1;
+			cout<<"ingresa el divisor "<<endl;
+			cin>>numero2;
+			if (numero2 != 0){//se hara uso de la funcion if para dar la respuesta 
+				resultado = numero1/ numero2;
+				cout << "El resultado es: " << resultado <<endl<<endl;
+			}else{//se dara el mensaje de error 
+				cout<<"no se puede dividir entre cero "<<endl<<endl;
+			}
+			cout<<"Presion cualquier tecla para regresar al menu ";//permitira al usuario volver a la pantalla del menu
+			_getch();// esperara que el usuario pricione cualquier tecla
+			system("cls");//limpiara la pantalla para que se vuelva a mostrar las opciones del menu
+			break;
+		case '3':
+			system("cls");
+			cout<<"   "<<"MULTIPLICACION "<<endl<<endl;
+			cout<<"ingrese el primer número "<<endl;
+			cin>>numero1;
+			cout<<"ingrese el segundo número "<<endl;
+			cin>>numero2;
+			cout<<"ingrese el tercer número "<<endl;
+			cin>>numero3;
+			resultado = numero1*numero2*numero3;
+			cout<<"El resultado de la multiplicacion es de :"<<resultado<<endl<<endl;
+			cout<<"Presion cualquier tecla para regresar al menu ";
+			_getch();
+			system("cls");
+			break;
+		case '4':
+			system("cls");
+			cout<<"MAYOR DE 3 MUNERO"<<endl<<endl;
+			cout<<"ingrese el primer número "<<endl;
+			cin>>numero1;
+			cout<<"ingrese el segundo número "<<endl;
+			cin>>numero2;
+			cout<<"ingrese el tercer número "<<endl;
+			cin>>numero3;
+			system("cls");
+			cout<<"MAYOR DE 3 MUNERO"<<endl<<endl;
+			// se hara uso del operador ternario ? para asignar el valor a la variable mayor //esto depende de los numeros introducido por el usuario 
+			mayor = (numero1 > numero2) ? ((numero1 > numero3) ? numero1 : numero3) : ((numero2 > numero3) ? numero2 : numero3);
+			cout << "El mayor de los tres numeros es: " << mayor << endl;
+			_getch();
+			system("cls");
+			break;
+		case 's'://Se puede ejecutar sin estas opciones de case pero se esta utilizando para que no muestre el mensaje de default cuando se presione la tecla S o s
+		case 'S':
+			cout<<"Finalizando programa";break;
+			
+			default:cout<< "error vuelve a intentarlo."<<" "<<"presiona cualquier tecla"<<endl;
+			_getch();
+			system("cls");
+			break;
+		}	//switch
+		/*el operador lógico && se utiliza para realizar una operación lógica de "Y" (AND) entre las dos condiciones.
+		El operador && evalúa como verdadero si ambas condiciones a su izquierda y derecha son verdaderas. 
+		nota: Si se puede utilizar una & pero al aser esto generara una advertencia lo ideal es utilizar las dos && //para no generar ninguna advertencia
+		*/
+	}while(minuscula !='S'&&minuscula !='s');
+			
+	return 0;
+}
+
